1e5ce5534aeeb5187b006428e224f40993c1ebd9
Jan Nowak <janeck@protonmail.com>
2020-05-24 08:58:16 +0100

    sub and multip tests

--- src/test/java/pl/sda/spring/CalculatorTest.java
+++ src/test/java/pl/sda/spring/CalculatorTest.java
@@ -27,4 +27,35 @@ public class CalculatorTest {
         //then
         Assert.assertEquals(5.0, actual, 0.00000001);
     }
+
+    @Test
+    public void subtractionTest(){
+
+        //given
+        double arg1 = 5.0;
+        double arg2 = 3.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.SUBTRACTION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(2.0, actual, 0.00000001);
+
+    }
+
+
+    @Test
+    public void multiplicationTest(){
+
+        //given
+        double arg1 = 5.0;
+        double arg2 = 3.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.MULTIPLICATION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(15.0, actual, 0.00000001);
+
+    }
 }
